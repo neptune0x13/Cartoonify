89e7ebe57b89a32910bed964a0c707bda71ed3f4
kunal-attri <attrikunal16@gmail.com>
2022-12-20 22:47:08 +0530

    converted into a class

--- cartoonify.py
+++ cartoonify.py
@@ -2,29 +2,31 @@ import cv2
 from numpy import ndarray
 
 
-def cartoonify(imagePath: str) -> ndarray:
-    """
-    Cartoonify Image
-    :param imagePath: path to original image
-    :return: Cartoonified Image
-    """
-    # Reading the Image
-    img = cv2.imread(imagePath)
+class Cartoonify:
+    @staticmethod
+    def cartoonify(imagePath: str) -> ndarray:
+        """
+        Cartoonify Image
+        :param imagePath: path to original image
+        :return: Cartoonified Image
+        """
+        # Reading the Image
+        img = cv2.imread(imagePath)
 
-    # Converting to Grayscale
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Converting to Grayscale
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Applying blur to image
-    img_blur = cv2.medianBlur(img_gray, 5)
+        # Applying blur to image
+        img_blur = cv2.medianBlur(img_gray, 5)
 
-    # Getting the edges
-    img_edges = cv2.adaptiveThreshold(img_blur, 255,
-                                      cv2.ADAPTIVE_THRESH_MEAN_C,
-                                      cv2.THRESH_BINARY, 9, 9)
+        # Getting the edges
+        img_edges = cv2.adaptiveThreshold(img_blur, 255,
+                                          cv2.ADAPTIVE_THRESH_MEAN_C,
+                                          cv2.THRESH_BINARY, 9, 9)
 
-    # Smoothening and Reducing noise w/o loss in edges
-    img_smooth = cv2.bilateralFilter(img, 9, 300, 300)
+        # Smoothening and Reducing noise w/o loss in edges
+        img_smooth = cv2.bilateralFilter(img, 9, 300, 300)
 
-    # Combining smoothened image and edges
-    img_cartoon = cv2.bitwise_and(img_smooth, img_smooth, mask=img_edges)
-    return img_cartoon
+        # Combining smoothened image and edges
+        img_cartoon = cv2.bitwise_and(img_smooth, img_smooth, mask=img_edges)
+        return img_cartoon
