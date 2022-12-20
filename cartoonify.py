f7c7c4aa67a46efc31c256a0a41df3d3dd6262c0
kunal-attri <attrikunal16@gmail.com>
2022-12-20 22:03:43 +0530

    Added proper comments in cartoonify.py

--- cartoonify.py
+++ cartoonify.py
@@ -7,13 +7,23 @@ def cartoonify(imagePath):
     :param imagePath: path to original image
     :return: Cartoonified Image
     """
-    orgImg = cv2.imread(imagePath)
-    # orgImg = cv2.cvtColor(orgImg, cv2.COLOR_RGB2RGB)
-    grayImg = cv2.cvtColor(orgImg, cv2.COLOR_BGR2GRAY)
-    smooth = cv2.medianBlur(grayImg, 5)
-    getEdge = cv2.adaptiveThreshold(smooth, 255,
-                                    cv2.ADAPTIVE_THRESH_MEAN_C,
-                                    cv2.THRESH_BINARY, 9, 9)
-    colorImg = cv2.bilateralFilter(orgImg, 9, 300, 300)
-    cartoon = cv2.bitwise_and(colorImg, colorImg, mask=getEdge)
-    return cartoon
+    # Reading the Image
+    img = cv2.imread(imagePath)
+
+    # Converting to Grayscale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Applying blur to image
+    img_blur = cv2.medianBlur(img_gray, 5)
+
+    # Getting the edges
+    img_edges = cv2.adaptiveThreshold(img_blur, 255,
+                                  cv2.ADAPTIVE_THRESH_MEAN_C,
+                                  cv2.THRESH_BINARY, 9, 9)
+
+    # Smoothening and Reducing noise w/o loss in edges
+    img_smooth = cv2.bilateralFilter(img, 9, 300, 300)
+
+    # Combining smoothened image and edges
+    img_cartoon = cv2.bitwise_and(img_smooth, img_smooth, mask=img_edges)
+    return img_cartoon
