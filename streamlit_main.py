1b5cc233ee06079c0a630dfe7cf2ac6f48cb0e47
Neptune 0x13 <rhythinraj06@gmail.com>
2022-12-21 16:59:47 +0530

    Added Cam

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,6 +1,5 @@
 import cv2
 import streamlit as st
-from PIL import Image
 from lib.cartoonify import Cartoonify
 
 cartoonifier = Cartoonify()
@@ -11,17 +10,19 @@ st.set_page_config(page_title="Cartoonifier",
 
 st.header("Cartoonifier")
 st.subheader("Here, you can click an image or upload one to cartoonify it!")
+st.write("### Click a picture - Click the picture through WebCam and cartoonify"
+         "### Upload Picture  - Upload an image from your computer and cartoonify")
 
-camera = st.button(label="Click Picture")
-if camera:
-    pass
-upload = st.file_uploader(label="Upload Picture", type=['jpg', 'png', 'jpeg'])
-print(type(upload))
-if upload:
-    open('images/tempFile.jpg', 'wb').write(upload.getvalue())
+camera = st.checkbox(label="Click Picture")
+
+
+def cartooner(pic):
+    with open('images/tempFile.jpg', 'wb') as f:
+        f.write(pic.getvalue())
+
     cartoonified = cartoonifier.cartoonify("images/tempFile.jpg")
     cv2.imwrite("images/cartoonified.png", cartoonified)
-    col1, col2 = st. columns(2)
+    col1, col2 = st.columns(2)
     with col1:
         st.image("images/tempFile.jpg", caption="Original Image")
     with col2:
@@ -29,3 +30,13 @@ if upload:
     st.download_button(label="Download Cartoonified Image",
                        data=open("images/cartoonified.png", "rb").read(),
                        file_name="cartoonified.png")
+
+
+if camera:
+    picture = st.camera_input("Take a picture")
+    if picture:
+        cartooner(picture)
+
+upload = st.file_uploader(label="Upload Picture", type=['jpg', 'png', 'jpeg'])
+if upload:
+    cartooner(upload)
